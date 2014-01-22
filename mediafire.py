b8709e906216b6c9f5204fa2fcd64258682e99f2
quanta <tonganhquan.net@gmail.com>
2014-01-22 10:33:02 +0700

    allow to download from .txt file including links

--- mediafire.py
+++ mediafire.py
@@ -3,10 +3,11 @@
 import os
 import sys
 import urllib2
+import time
 from BeautifulSoup import BeautifulSoup
 
 
-def download(quickkey, dir):
+def download(filename, quickkey, dir):
     mediafire_url = "http://www.mediafire.com/?"
     get_info_api = "http://www.mediafire.com/api/file/get_info.php?quick_key="
     url = ''.join([mediafire_url, quickkey])
@@ -22,10 +23,6 @@ def download(quickkey, dir):
     
     print dl_link
     
-    file_info = urllib2.urlopen(''.join([get_info_api, quickkey]))
-    file_soup = BeautifulSoup(file_info)
-    filename = file_soup.find('filename').string
-
     u = urllib2.urlopen(dl_link)
     f = open(os.path.join(dir, filename), 'wb')
     print 'Downloading %s...' % filename
@@ -34,15 +31,7 @@ def download(quickkey, dir):
     print '%s was saved to the %s.' % (filename, dir)
 
 
-def get_links():
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print "Usage: %s <url> <directory>" % sys.argv[0]
-        sys.exit(1)
-    url = sys.argv[1]
-    if len(sys.argv) == 3:
-        dir = sys.argv[2]
-    else:
-        dir = os.getcwd()
+def get_links(url, dir):
     folder_key = url.split("?")[1]
     page = urllib2.urlopen("http://www.mediafire.com/api/folder/get_content.php?folder_key=%s&content_type=files" % folder_key)
     soup = BeautifulSoup(page)
@@ -50,7 +39,27 @@ def get_links():
     for f in files:
         filename = f.find('filename').string
         quickkey = f.find('quickkey').string
-        download(quickkey, dir)
+        download(filename, quickkey, dir)
+        time.sleep(3)
+
+
+def main():
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print "Usage: %s <url> <directory>" % sys.argv[0]
+        sys.exit(1)
+    if len(sys.argv) == 3:
+        dir = sys.argv[2]
+    else:
+        dir = os.getcwd()
+    if sys.argv[1].startswith("http://www.mediafire.com/?"):
+        url = sys.argv[1]
+        get_links(url, dir)
+    else:
+        file = sys.argv[1].encode('string-escape')
+        with open(file) as f:
+            for url in f:
+                get_links(url.strip(), dir)
+
 
 if __name__ == '__main__':
-    get_links()
+    main()
