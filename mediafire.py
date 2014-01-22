ba0fb37e11c16ad4406cb381a59e6f30141e197f
quanta <tonganhquan.net@gmail.com>
2014-01-22 11:39:30 +0700

    add the text progress bar when downloading

--- mediafire.py
+++ mediafire.py
@@ -5,15 +5,14 @@ import sys
 import urllib2
 import time
 from BeautifulSoup import BeautifulSoup
+from hurry.filesize import size
 
 
 def download(filename, quickkey, dir):
     mediafire_url = "http://www.mediafire.com/?"
-    get_info_api = "http://www.mediafire.com/api/file/get_info.php?quick_key="
     url = ''.join([mediafire_url, quickkey])
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page)
-    print 'Parsing the html to find the download_link class...'
     func = soup.find('div', attrs={'class':'download_link'}).text
     print 'Getting the direct link...'
     for line in func.splitlines():
@@ -25,10 +24,26 @@ def download(filename, quickkey, dir):
     
     u = urllib2.urlopen(dl_link)
     f = open(os.path.join(dir, filename), 'wb')
-    print 'Downloading %s...' % filename
-    f.write(u.read())
+    meta = u.info()
+    filesize = int(meta.getheaders("Content-Length")[0])
+    print 'Downloading "{0}" ({1}) ...'.format(filename, size(filesize))
+
+    filesize_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        filesize_dl += len(buffer)
+        f.write(buffer)
+        p = float(filesize_dl) / filesize
+        status = r"{0} [{1:.2%}]".format(filesize_dl, p)
+        status = status + chr(8)*(len(status)+1)
+        sys.stdout.write(status)
+
     f.close()
-    print '%s was saved to the %s.' % (filename, dir)
+    print 'Done. "%s" was saved to the %s.\n' % (filename, dir)
 
 
 def get_links(url, dir):
@@ -53,7 +68,15 @@ def main():
         dir = os.getcwd()
     if sys.argv[1].startswith("http://www.mediafire.com/?"):
         url = sys.argv[1]
-        get_links(url, dir)
+        quickkey = sys.argv[1].split('?')[-1]
+        if len(quickkey) == 15:
+            get_info_api = "http://www.mediafire.com/api/file/get_info.php?quick_key="
+            page = urllib2.urlopen(''.join([get_info_api, quickkey]))
+            soup = BeautifulSoup(page)
+            filename = soup.find('filename').string
+            download(filename, quickkey, dir)
+        elif len(quickkey) == 13:
+            get_links(url, dir)
     else:
         file = sys.argv[1].encode('string-escape')
         with open(file) as f:
